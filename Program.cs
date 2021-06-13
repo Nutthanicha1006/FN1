f21b23c56fd20e27a9fdc685d331421d54fcd0a2
LAPTOP-2QNNL6VT\Lenovo <nutthanicha.kmutt@mail.kmutt.ac.th>
2021-06-13 16:34:16 +0700

    no message

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FinalTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X;
+            int[,] num = new int[3, 3];
+            
+            for (int i = 0; i < num.Length; i++)
+            {
+                for (int j = 0; j < num.Length; j++)
+                {
+                    num[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
